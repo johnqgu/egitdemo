3eb053481ef5002719df9e768d345ced02b6aa3b
Gu, John <John.Gu@gwl.ca>
2018-12-11 16:17:09 -0600

    updated

--- HelloWorld/src/com/jgu/git/test1/TestGit.java
+++ HelloWorld/src/com/jgu/git/test1/TestGit.java
@@ -3,7 +3,7 @@ package com.jgu.git.test1;
 public class TestGit {
 
 	public static void main(String[] args) {
-		String returnCode = "Hello";
+		String returnCode = "Hello1";
 		System.out.println(returnCode);
 	}
 
