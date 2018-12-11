9d75116d62ab20c44d1fe4e460dcae7b544fcf2e
Gu, John <John.Gu@gwl.ca>
2018-12-11 16:00:49 -0600

    Second commit

--- HelloWorld/src/com/jgu/git/test1/TestGit.java
+++ HelloWorld/src/com/jgu/git/test1/TestGit.java
@@ -3,8 +3,8 @@ package com.jgu.git.test1;
 public class TestGit {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String returnCode = "Hello";
+		System.out.println(returnCode);
 	}
 
 }
